0983acc962546b17408cee956848fd84a2bd557d
mkietlinski <m.kietlinski@steambytes.com>
2015-07-24 23:48:18 +0200

    Weapon

---
+++ Assets/Scripts/Player/PlayerWeapon.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class PlayerWeapon : MonoBehaviour
+    {
+        [SerializeField]
+        private float bulletSpawningDistanceOffset;
+        [SerializeField]
+        private Vector2 bulletSpawningDirection;
+        [SerializeField] 
+        private GameObject bulletPrefab;
+        [SerializeField] 
+        private float firePower;
+
+        public void Shoot()
+        {
+            Vector3 finalSpawnPosition = new Vector3(gameObject.transform.position.x + bulletSpawningDirection.x * bulletSpawningDistanceOffset,
+                                                        gameObject.transform.position.y + bulletSpawningDirection.y * bulletSpawningDistanceOffset,
+                                                        gameObject.transform.position.z);
+            GameObject go = Instantiate(bulletPrefab, finalSpawnPosition, bulletPrefab.transform.rotation) as GameObject;
+            go.GetComponent<Rigidbody2D>().AddForce(bulletSpawningDirection*firePower);
+        }
+
+        public void Update()
+        {
+            //QQ
+            if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+            {
+                bulletSpawningDirection = Vector2.right;
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+            {
+                bulletSpawningDirection = Vector2.up;
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+            {
+                bulletSpawningDirection = Vector2.down;
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+            {
+                bulletSpawningDirection = Vector2.left;
+            }
+            //QQ
+
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                Shoot();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Weapons/BulletBase.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Weapons
+{
+    public class BulletBase : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.layer == LayerMask.GetMask("Player"))
+            {
+                Debug.Log("Hit Player");
+            }
+        }
+    }
+}
