0b1843f4c8eccf4a846050bde7847b4b74d9c7d0
mkietlinski <m.kietlinski@steambytes.com>
2015-07-25 14:02:26 +0200

    UI

---
+++ Assets/Scripts/GUI/JoinScreenControler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.GUI
+{
+    public class JoinScreenControler : GUIScreen
+    {
+        NetworkClient myClient;
+
+        public InputField IPfield;
+
+        public void Connect()
+        {
+            myClient = new NetworkClient();
+            myClient.RegisterHandler(MsgType.Connect, OnConnected);
+            myClient.Connect(IPfield.text, 7777);
+        }
+
+        public void OnConnected(NetworkMessage netMsg)
+        {
+            GUIManager.instance.HideCurrentScreen();
+        }
+    }
+}
---
+++ Assets/Scripts/GUI/MainMenuScreenController.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine.Networking;
+
+namespace Assets.Scripts.GUI
+{
+    public class MainMenuScreenController : GUIScreen
+    {
+        NetworkClient myClient;
+
+        private void HostServer()
+        {
+            NetworkServer.Listen(7777);
+        }
+
+        private void ConnectLocalClient()
+        {
+            myClient = ClientScene.ConnectLocalServer();
+            myClient.RegisterHandler(MsgType.Connect, OnConnected);
+            GUIManager.instance.HideCurrentScreen();
+        }
+
+        public void OnConnected(NetworkMessage netMsg)
+        {
+            GUIManager.instance.HideCurrentScreen();
+        }
+
+        public void HostAndConnect()
+        {
+            HostServer();
+            ConnectLocalClient();
+        }
+    }
+}
