3ab65188819a4cd7a3bf147b0dec126af5cee8da
Valar92 <jakub.skalecki@gmail.com>
2015-07-25 23:53:06 +0200

    Trap activators rework

---
+++ Assets/Scripts/Traps/TrapActivatorProximity.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Scripts.Traps;
+using UnityEngine;
+using System.Collections;
+
+public class TrapActivatorProximity : TrapActivator
+{
+    private int playersCountOnActivator;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        playersCountOnActivator++;
+        SetActivatorAsTriggered();
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        playersCountOnActivator--;
+        if (playersCountOnActivator == 0)
+        {
+            SetActivatorAsNotTriggered();
+        }
+    }
+}
