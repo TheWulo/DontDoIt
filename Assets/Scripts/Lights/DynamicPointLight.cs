63c9f58e3bcaed615e8f232bf36347f91392f494
mkietlinski <m.kietlinski@steambytes.com>
2015-07-26 01:32:17 +0200

    Random Team Skins

---
+++ Assets/Scripts/Lights/DynamicPointLight.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Lights
+{
+    public class DynamicPointLight : MonoBehaviour
+    {
+        public Light PointLight;
+
+        public float blinkingSpeed;
+
+        private bool increasing;
+        private bool decreasing;
+
+        private void Start()
+        {
+            increasing = true;
+            decreasing = false;
+        }
+
+        private void Update()
+        {
+            if (increasing)
+            {
+                PointLight.intensity += blinkingSpeed*Time.deltaTime;
+                if (PointLight.intensity >= 8)
+                {
+                    increasing = false;
+                    decreasing = true;
+                }
+            }
+            if (decreasing)
+            {
+                PointLight.intensity -= blinkingSpeed * Time.deltaTime;
+                if (PointLight.intensity <= 4)
+                {
+                    increasing = true;
+                    decreasing = false;
+                }
+            }
+        }
+            
+        }
+}
