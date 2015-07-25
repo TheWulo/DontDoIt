9c6e7fd56b207eadf904ccec1dc99be7308969ce
mkietlinski <m.kietlinski@steambytes.com>
2015-07-25 19:41:23 +0200

    Animations Working in All directions

---
+++ Assets/Scripts/Player/AnimationController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class AnimationController : MonoBehaviour
+    {
+        public GameObject GFXObject;
+        public Animator GFXAnimator;
+
+        public void Update()
+        {
+            if (Input.GetAxis("Horizontal") >= 0.1)
+            {
+                if (GFXObject.gameObject.transform.localScale.x > 0)
+                    GFXObject.gameObject.transform.localScale = new Vector3(GFXObject.gameObject.transform.localScale.x * -1, GFXObject.gameObject.transform.localScale.y, GFXObject.gameObject.transform.localScale.z);
+            }
+            if (Input.GetAxis("Horizontal") <= -0.1)
+            {
+                if (GFXObject.gameObject.transform.localScale.x < 0)
+                    GFXObject.gameObject.transform.localScale = new Vector3(GFXObject.gameObject.transform.localScale.x * -1, GFXObject.gameObject.transform.localScale.y, GFXObject.gameObject.transform.localScale.z);
+            }
+
+            if (Input.GetAxis("Horizontal") >= 0.1 || Input.GetAxis("Horizontal") <= -0.1)
+            {
+                GFXAnimator.Play("Run");
+            }
+            else
+            {
+                GFXAnimator.Play("Idle");
+            }
+        }
+    }
+}
