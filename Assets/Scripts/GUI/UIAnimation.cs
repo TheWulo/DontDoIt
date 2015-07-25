0e2ef7cfbf709720b42d1f3fbaf124ac40e9d801
mkietlinski <m.kietlinski@steambytes.com>
2015-07-25 17:10:28 +0200

    background

---
+++ Assets/Scripts/GUI/UIAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.GUI
+{
+    public class UIAnimation : MonoBehaviour
+    {
+        public Image targetImage;
+
+        public List<Sprite> SpritesList;
+
+        public float timeBetweenSprites;
+        private float timer;
+        private int currentSpriteIndex;
+
+        private void Update()
+        {
+            timer += Time.deltaTime;
+            if (timer > timeBetweenSprites)
+            {
+                currentSpriteIndex++;
+                if (currentSpriteIndex >= SpritesList.Count)
+                    currentSpriteIndex = 0;
+
+                targetImage.sprite = SpritesList[currentSpriteIndex];
+                timer = 0;
+            }
+        }
+
+    }
+}
