d7d48eac666c91f8060df60df8e2c9721496c4a0
mkietlinski <m.kietlinski@steambytes.com>
2015-07-26 10:04:43 +0200

    fix

---
+++ Assets/Scripts/Player/ParticleEffects.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class ParticleEffects : MonoBehaviour
+    {
+        public ParticleSystem BloodParticleSystem;
+
+        public void PlayBlood()
+        {
+            BloodParticleSystem.Play();
+        }
+    }
+}
