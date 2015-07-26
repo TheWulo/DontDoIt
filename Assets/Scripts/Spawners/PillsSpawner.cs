3239ca3f6b347a073c8b49f20f10b02bb0d81422
Emilia <szymanska.emilka@gmail.com>
2015-07-26 05:12:53 +0200

    niedzialajace pilsy

---
+++ Assets/Scripts/Spawners/PillsSpawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Traps;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using System;
+using Random = System.Random;
+
+public class PillsSpawner : NetworkBehaviour
+{
+    [SerializeField]
+    protected List<TrapPills> PillsOnScene;
+    [SerializeField]
+    protected float Cooldown;
+    private Random random = new System.Random();
+
+
+    void Start()
+    {
+        if (isServer)
+            PillsOnScene[0].gameObject.SetActive(false);
+    }
+
+    public void Collected()
+    {
+        if (!isServer) return;
+        PillsOnScene.ForEach(x => x.gameObject.SetActive(false));
+        StartCoroutine(WaitCooldown());
+    }
+
+    private IEnumerator WaitCooldown()
+    {
+        yield return new WaitForSeconds(Cooldown);
+        PillsOnScene[random.Next(0, 2)].gameObject.SetActive(true);
+    }
+}
--- Assets/Scripts/Traps/TrapPills.cs
+++ Assets/Scripts/Traps/TrapPills.cs
@@ -1,9 +1,35 @@
-﻿
+﻿using Assets.Scripts.Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
 
 namespace Assets.Scripts.Traps
 {
     public class TrapPills : TrapBase
     {
+        [SerializeField]
+        protected float KillDelay;
+        [SerializeField]
+        protected PillsSpawner Spawner;
+        // Use this for initialization
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            var player = other.GetComponent<PlayerBase>();
+            if (player && player.isLocalPlayer)
+            {
+                StartCoroutine(DelayedDeath(player));
+                Spawner.Collected();
+            }
+        }
+
+        private IEnumerator DelayedDeath(PlayerBase playerBase)
+        {
+            yield return new WaitForSeconds(KillDelay);
+            if(isServer)
+            playerBase.RpcDie(DeathReason.TrapSpike);
+        }
 
     }
 }
